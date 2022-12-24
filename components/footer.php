cfbba73c976b5a181f74509d6a30a46049f02a6d
KalpaniGajanayaka <89629736+KalpaniGajanayaka@users.noreply.github.com>
2022-12-24 23:37:12 +0530

    commit all components
    commit

---
+++ components/footer.php
@@ -0,0 +1,35 @@
+<footer class="footer">
+   <section class="grid">
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="index.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact us</h3>
+         <a href="076 7746778"><i class="fas fa-phone"></i> +94 76 7746778</a>
+         <a href="0452256770"><i class="fas fa-phone"></i> +94 45 2256770</a>
+         <a href="delafoodcity@gmail.com"><i class="fas fa-envelope"></i> delafoodcity@gmail.com</a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i> Weddagala road, Kalawana, Rathnapura </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+
+      </div>
+   </section>
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Kalpani Gajanayaka</span> | all rights reserved!</div>
+</footer>
